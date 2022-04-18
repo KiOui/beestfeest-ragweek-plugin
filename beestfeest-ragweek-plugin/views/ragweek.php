d7a777feb627ab917a651a5789a2b890f08ebfc1
Lars van Rhijn <larsvanrhijn@gmail.com>
2022-04-18 14:14:43 +0200

    Changed CSS and template file

--- beestfeest-ragweek-plugin/views/ragweek.php
+++ beestfeest-ragweek-plugin/views/ragweek.php
@@ -10,7 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 wp_enqueue_script( 'bfrw-ragweek-script', BFRW_PLUGIN_URI . 'assets/js/board.js', array( 'jquery' ), '1.0', true );
 wp_enqueue_script( 'jquery' );
-wp_enqueue_style( 'brfw-styles', BFRW_PLUGIN_URI . 'assets/css/ragweek.css', array(), '1.0' );
+wp_enqueue_style( 'brfw-styles', BFRW_PLUGIN_URI . 'assets/css/ragweek.css', array(), '1.1' );
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
@@ -21,26 +21,27 @@ wp_enqueue_style( 'brfw-styles', BFRW_PLUGIN_URI . 'assets/css/ragweek.css', arr
 	</head>
 	<body <?php body_class(); ?>>
 	<?php wp_body_open(); ?>
-
 	<div class="bfrw-wrapper">
-		<video autoplay muted loop class="background-video" preload="auto">
-			<source src="file:///tmp/backgroundvideo.mp4" type="video/mp4">
-			<source src="<?php echo esc_attr( BFRW_PLUGIN_URI . 'assets/mp4/backgroundvideo.mp4' ); ?>" type="video/mp4">
-			Your browser does not support HTML5 video.
-		</video>
-	</div>
+		<div class="bfrw-video-wrapper">
+			<video autoplay muted loop class="background-video" preload="auto">
+				<source src="file:///tmp/backgroundvideo.mp4" type="video/mp4">
+				<source src="<?php echo esc_attr( BFRW_PLUGIN_URI . 'assets/mp4/backgroundvideo.mp4' ); ?>" type="video/mp4">
+				Your browser does not support HTML5 video.
+			</video>
+		</div>
 
-	<img src="<?php echo esc_attr( BFRW_PLUGIN_URI . 'assets/img/beestfeestletters.png' ); ?>" class="bfrw-top-centered" />
-	<div class="bfrw-songs">
-		<ul id="songs">
-		</ul>
-	</div>
+		<img src="<?php echo esc_attr( BFRW_PLUGIN_URI . 'assets/img/beestfeestletters.png' ); ?>" class="bfrw-top-centered" />
+		<div class="bfrw-songs">
+			<ul id="songs">
+			</ul>
+		</div>
 
-	<div class="bfrw-bottom-fixed">
-		<img src="<?php echo esc_attr( BFRW_PLUGIN_URI . 'assets/img/gradient.png' ); ?>" class="gradient" />
-		<div class="full-width">
-			<div class="messages">
-				<marquee behavior="scroll" direction="left" class="messages-scroller" id="scroller"></marquee>
+		<div class="bfrw-bottom-fixed">
+			<img src="<?php echo esc_attr( BFRW_PLUGIN_URI . 'assets/img/gradient.png' ); ?>" class="gradient" />
+			<div class="full-width">
+				<div class="messages">
+					<marquee behavior="scroll" direction="left" class="messages-scroller" id="scroller"></marquee>
+				</div>
 			</div>
 		</div>
 	</div>
